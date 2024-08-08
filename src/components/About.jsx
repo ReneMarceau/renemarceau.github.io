10b303ef3f413772c19296e126da6ac3b8467c85
ReneMarceau <rene.marceau.dev@gmail.com>
2024-08-08 17:40:35 -0400

    Configure React app for deployment to GitHub Pages

---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        I'm a full stack developer with a strong focus on AI, frontend development using React,
+        and backend architecture with Django & Node.js. I work closely with clients to understand their unique 
+        needs and deliver user-friendly applications for their tailored solutions.
+        Whether you're a business looking to enhance your digital presence or solve complex challenges.
+        I'll guide you every step of the way, ensuring that we achieve the best possible results together.
+      </motion.p>
+
+      <div className="mt-20 flex justify-center flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,258 @@
+import { data } from 'autoprefixer';
+import {
+  frontend,
+  database,
+  backend,
+  ux,
+  user,
+  prototyping,
+  javascript,
+  python,
+  svelte,
+  mysql,
+  mango,
+  firebase,
+  express,
+  django,
+  aws,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  s42,
+  codeur,
+  upwork,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Development',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Development',
+    icon: backend,
+  },
+  {
+    title: 'Database Design & Management',
+    icon: database,
+  },
+  {
+    title: 'Client Consultations',
+    icon: user,
+  },
+];
+
+const technologies = [
+  {
+    name: 'Python',
+    icon: python,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Svelte',
+    icon: svelte,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Express JS',
+    icon: express,
+  },
+  {
+    name: 'Django',
+    icon: django,
+  },
+  {
+    name: 'PostgreSQL',
+    icon: postgresql,
+  },
+  {
+    name: 'MySQL',
+    icon: mysql,
+  },
+  {
+    name: 'Firebase',
+    icon: firebase,
+  },
+  {
+    name: 'AWS',
+    icon: aws,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: 'FreeLancer',
+    company_name: 'codeur.com',
+    icon: codeur,
+    iconBg: '#333333',
+    date: 'Mar 2021 - Sep 2022',
+  },
+  {
+    title: 'Student - Software Engineering',
+    company_name: '42 School',
+    icon: s42,
+    iconBg: '#333333',
+    date: 'Sep 2022 - SEP 2024',
+  },
+  {
+    title: 'Lead Web Developer',
+    company_name: 'Creme de la Creme',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Nov 2023 - Mar 2024',
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Upwork',
+    icon: upwork,
+    iconBg: '#333333',
+    date: 'Sep 2024 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Transcendence',
+    description: 'My last 42 School project, a website to play pong against an AI.',
+    tags: [
+      {
+        name: 'django',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'postgresql',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'docker',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/ReneMarceau/ft_transcendence',
+    demo: '#',
+  },
+  {
+    id: 'project-2',
+    name: 'Projeckt AI',
+    description: 'AI-driven platform to evaluate, refine, and guide entrepreneurs through their business journey.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/ReneMarceau/projeckt-dashboard',
+    demo: 'https://www.projecktai.com/',
+  },
+  {
+    id: 'project-3',
+    name: 'Videos Blade',
+    description:
+      'A Youtube video analytics platform to get a detailed analysis of any video on Youtube.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/ReneMarceau/Videos_Blade',
+    demo: 'https://videosblade.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Maconnerie Pro',
+    description: 'A single-page application for a masonry company.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'sass',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/ReneMarceau/maconneriepro',
+    demo: 'https://www.maconneriepro.com/',
+  },
+];
+
+export { services, technologies, experiences, projects };
